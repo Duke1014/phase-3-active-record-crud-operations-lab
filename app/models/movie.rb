e28bd8859d7befb5fc057a5462bb34db1f755a3b
Duke Norsworthy <duke.norsworthy@gmail.com>
2021-11-08 17:32:45 -0500

    commit tree ee318aa14cd5a9878807da2c75b809b85aa80c7d

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,51 @@
 class Movie < ActiveRecord::Base
 
+    # Create
+    def self.create_with_title(title)
+        Movie.create(title: title)
+    end
+
+    # Read
+    def self.first_movie
+
+    end
+
+    def self.last_movie
+
+    end
+
+    def self.movie_count
+
+    end
+
+    def self.find_movie_with_id
+
+    end
+
+    def self.find_movie_with_attributes
+
+    end
+
+    def self.find_movies_after_2002
+
+    end
+
+    # Update
+    def update_with_attributes
+
+    end
+
+    def self.update_all_titles
+
+    end
+
+    # Delete
+    def self.delete_by_id
+
+    end
+
+    def self.delete_all_movies
+
+    end
+
 end
